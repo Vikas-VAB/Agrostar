25c2888d1c886ec34ce1710ace04906f545287fb
Vikas-VAB <79959848+Vikas-VAB@users.noreply.github.com>
2022-07-10 14:59:02 +0530

    backups

---
+++ bitbucket.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+USERNAME=vikas_bodke
+PASSWORD=nHV6HxFbB6ZTPcRYZpqe
+filename=allreposdata.txt
+
+read -p "you want to give repositories using repositories.txt file (type file) or through command line input (type stdin ):" option
+
+if [ "$option" = "stdin" ]
+then
+	read -p "Enter repositories names separated by space: " repos_names
+
+	for repo in $repos_names
+	do
+
+	curl -XPOST -u "$USERNAME:$PASSWORD" -H "Content-type: application/json" "https://api.bitbucket.org/2.0/repositories/agrostar/$repo/deploy-keys" -d @keys.json
+
+	done
+ 
+elif [ "$option" = "file" ]
+then
+	while read line; do
+        curl -u "$USERNAME:$PASSWORD" -H "Content-type: application/json" "https://api.bitbucket.org/2.0/repositories/agrostardata/$line/deploy-keys" 
+
+        done < $filename
+else 
+	echo " type file or stdin "
+fi
+
